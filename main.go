57f6cd24f0649221cf407a7e8fce578696ba3487
sabnaj <sabnaj@appscode.com>
2025-05-21 18:54:15 +0600

    clice practice
    Signed-off-by: sabnaj <sabnaj@appscode.com>

--- main.go
+++ main.go
@@ -3,8 +3,51 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/Sabnaj-42/BookServer-API/cmd"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func f1() {
+	fmt.Println("hello world f1")
+}
+
+func f2(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("-----------writing value to channel-------------------")
+
+	ch <- 1
+	fmt.Println("-----------writing Done-------------------")
+	fmt.Println("hello world f2")
+}
+
+func f3(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("waiting to receive value from channelchannel")
+	time.Sleep(4 * time.Second)
+	a := <-ch
+	fmt.Println("Read done########################")
+	fmt.Println("received value from channel", a)
+}
+
+func f4() {
+	fmt.Println("hello world f3")
+}
 
 func main() {
-	cmd.Execute()
+	sl := make([]int, 5)
+	for i := 0; i < 5; i++ {
+		sl = append(sl, rand.Int())
+	}
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go f2(ch, &wg)
+	wg.Add(1)
+	go f3(ch, &wg)
+	time.Sleep(25 * time.Second)
+	wg.Wait()
+
 }
